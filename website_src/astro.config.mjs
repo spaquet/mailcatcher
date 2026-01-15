29d2c16988968bde573937332fcd109b85f823dc
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2026-01-15 14:25:24 -0800

    New Astro site

--- website_src/astro.config.mjs
+++ website_src/astro.config.mjs
@@ -1,21 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://spaquet.github.io/mailcatcher/',
-  base: '/',
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-    }),
-  ],
   vite: {
-    plugins: [tailwindcss()],
+      plugins: [tailwindcss()],
   },
-  output: 'static',
-  trailingSlash: 'ignore',
-  compressHTML: true,
+
+  integrations: [icon()],
 });
